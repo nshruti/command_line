2b75801357d19e256d64d280ecf9256bad2dd22d
Shruti Nagarkanti <nagarkantishruti@gmail.com>
2016-11-12 23:12:38 -0500

    useful command snippets

--- useful_snippets.sh
+++ useful_snippets.sh
@@ -1,1 +1,10 @@
  sed -i 's/original/new/g' file.txt
+
+# Explanation:
+# sed = Stream EDitor
+# -i = in-place (i.e. save back to the original file)
+# s = the substitute command
+# original = a regular expression describing the word to replace (or just the word itself)
+# new = the text to replace it with
+# g = global (i.e. replace all and not just the first occurrence)
+# file.txt = the file name
